13826495170c60e1c3a7470791834ecdcb334c95
chenyanqi <chenyanqi987@163.com>
2016-11-25 10:04:46 +0800

    1

---
+++ DMGuideView/DMGuideView/DMGuideView.h
@@ -0,0 +1,13 @@
+//
+//  DMGuideView.h
+//  DMGuideView
+//
+//  Created by 陈彦岐 on 2016/11/25.
+//  Copyright © 2016年 陈彦岐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMGuideView : UIView
+
+@end
---
+++ DMGuideView/DMGuideView/DMGuideView.m
@@ -0,0 +1,21 @@
+//
+//  DMGuideView.m
+//  DMGuideView
+//
+//  Created by 陈彦岐 on 2016/11/25.
+//  Copyright © 2016年 陈彦岐. All rights reserved.
+//
+
+#import "DMGuideView.h"
+
+@implementation DMGuideView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
